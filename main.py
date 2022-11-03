91f33e075df21e40836f7a4db7c1f5b78854c01a
Carlos Marcelo Moro Filho <100219632+cmarcelomoro@users.noreply.github.com>
2022-11-03 16:33:40 -0300

    teste 5

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ st.title("Título da página") ##título da página (texto que aparece em cima n
 
 st.subheader("Subheader") ## subheader da página(aparece abaixo do título)
 
+st.header("Header") 
 
-
+st.text("Página com interface utilizando streamlit") ## parágrafo
